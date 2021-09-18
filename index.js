c83fb1133918f290841422b05e632948cfaaebda
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2021-09-18 23:54:59 +0200

    yeah

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// imports shit and the client thing
-const { Client, Intents } = require('discord.js');
+// Import discord.js and create the client
+const Discord = require('discord.js')
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 const { token } = require('./credentials.json');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// logger if everythings okie dokie
-client.once('ready', () => {
-	console.log('Everything okie dokei');
-});
-//welcome message
-client.on('guildMemberAdd', member => {
-    member.guild.channels.get('channelID').send("Welcome you cumstain"); 
-});
+// Register an event so that when the bot is ready, it will log a messsage to the terminal
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+})
 
-//login what else ?
+
+// client.login logs the bot in and sets it up for use. You'll enter your token here.
 client.login(token);
