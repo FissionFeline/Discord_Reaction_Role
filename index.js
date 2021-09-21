66a5ea420e18f1ba80b040260798232a50f7e018
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2021-09-21 21:59:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ client.on('messageReactionRemove', async (reaction, user,message) => {
   console.log(user.id);
   const myGuild = client.guilds.cache.get('796557651206864916');
   const myRole = myGuild.roles.cache.find(role => role.name === 'red');
-  user.roles.add(myRole);
+  const User = client.users.cache.get(user.id); 
+  User.roles.add(myRole);
 });
 client.login(token);
