d5dbc9789d77c62367e1855ad9d7226396156c99
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2021-09-21 21:26:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const { token } = require('./credentials.json');
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
-
+client.on('ready', () => {
+	console.log(`We are in as ${client.user.tag}!`);
+  })
+  
 client.on('messageReactionAdd', async (reaction, user,message) => {
+	if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message:', error);
+			return;
+		}
+	}
+  console.log(user.id);
+});
+client.on('messageReactionRemove', async (reaction, user,message) => {
 	if (reaction.partial) {
 		try {
 			await reaction.fetch();
